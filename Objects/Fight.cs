fbd9bd4986d9be0b9af8fd29b9bc2aabf097de31
SRC-11 <sophia.cattelan@cuw.edu>
2025-03-24 08:51:17 -0500

    Update Fight.cs

--- Objects/Fight.cs
+++ Objects/Fight.cs
@@ -19,53 +19,5 @@ public class Fight
 
     }
 
-    public void startFight()
-    {
-        //should have the attacker and defender fight each until one of them dies.
-        //the attacker and defender should alternate between each fight round and
-        //the one who goes first was determined in the constructor.
-        	Player currentAttacker = this.attacker;
-		Player currentDefender = this.defender;
-		
-
-		while(true) 
-		{
-		    this.attacker.display();
-		    this.defender.display();
-		    
-		
-		     int d4 = Random.Range(0, 15) + 1;
-			if(attackRoll >= currentDefender.getHP())
-			{
-		
-				int damage = d4;
-				currentDefender.takeDamage(damage);
-
-				if(currentDefender.isDead())
-				{
-					System.out.println(currentAttacker.getName() + " has won the fight!");
-					break; 
-				}
-			}
-			else
-			{
-			
-				System.out.println(currentAttacker.getName() + " swings and misses");
-
-				if(attackRoll == 1)
-				{
-					System.out.println("**** Critical Miss **** " + currentAttacker.getName() + " stumbles and stabs themselves in the foot!");
-				}
-			}
-
-		
-			Player temp = currentAttacker;
-			currentAttacker = currentDefender;
-			currentDefender = temp;
-		} 
-		
-		
-		System.out.println("Game Over!!!!");
-		
-    }
+  
 }
